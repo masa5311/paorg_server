89973a688d364005cf364a4ed51ed02da01b56a3
masa <masa.docomo.ne.jp@y7.dion.ne.jp>
2021-09-26 05:53:53 +0900

    Firebase認証処理追加 ・FirabaseSDK追加 ・FirebaseSDK初期化処理追加 ・Firebase認証処理追加

--- src/main/java/com/paorg/paorg_server/PaorgServerApplication.java
+++ src/main/java/com/paorg/paorg_server/PaorgServerApplication.java
@@ -1,13 +1,31 @@
 package com.paorg.paorg_server;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class PaorgServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PaorgServerApplication.class, args);
+    initializeFirebaseApp();
+  }
+
+  private static void initializeFirebaseApp() {
+    FirebaseOptions options = null;
+    try {
+      options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.getApplicationDefault())
+        .build();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    FirebaseApp.initializeApp(options);
   }
 
 }
--- src/main/java/com/paorg/paorg_server/controller/TestController.java
+++ src/main/java/com/paorg/paorg_server/controller/TestController.java
@@ -1,8 +1,8 @@
 package com.paorg.paorg_server.controller;
 
-// import com.google.firebase.auth.FirebaseAuth;
-// import com.google.firebase.auth.FirebaseAuthException;
-// import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +15,15 @@ public class TestController {
   @GetMapping
   @CrossOrigin("http://localhost:3000")
   public String get(HttpServletRequest request) {
-    // String idToken = request.getHeader("Authorization");
+    String idToken = request.getHeader("Authorization");
 
-    // try {
-    //   FirebaseToken decodedToken =
-    //     FirebaseAuth.getInstance().verifyIdToken(idToken);
-    //   return "サーバー認証成功：java_firebase";
-    // } catch (FirebaseAuthException e) {
-    //   e.printStackTrace();
-    //   return "サーバー認証失敗";
-    // }
-    return "OK";
+    try {
+      FirebaseToken decodedToken =
+        FirebaseAuth.getInstance().verifyIdToken(idToken);
+      return "サーバー認証成功：java_firebase";
+    } catch (FirebaseAuthException e) {
+      e.printStackTrace();
+      return "サーバー認証失敗";
+    }
   }
 }
