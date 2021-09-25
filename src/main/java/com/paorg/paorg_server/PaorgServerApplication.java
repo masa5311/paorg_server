17803d3c2f665632bc95cbab5ea768b5c9f6d67e
masa <masa.docomo.ne.jp@y7.dion.ne.jp>
2021-09-26 03:47:48 +0900

    初期コミット

---
+++ src/main/java/com/paorg/paorg_server/PaorgServerApplication.java
@@ -0,0 +1,13 @@
+package com.paorg.paorg_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaorgServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaorgServerApplication.class, args);
+  }
+
+}
