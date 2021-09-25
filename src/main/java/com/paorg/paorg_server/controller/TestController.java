d8aeb00f2492b25b5f6bdd4608758c3db8d3ad56
masa <masa.docomo.ne.jp@y7.dion.ne.jp>
2021-09-26 04:55:20 +0900

    設定を追加（起動/接続確認） ・datasource追加 ・logging設定追加 ・テスト用コントローラー追加

---
+++ src/main/java/com/paorg/paorg_server/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.paorg.paorg_server.controller;
+
+// import com.google.firebase.auth.FirebaseAuth;
+// import com.google.firebase.auth.FirebaseAuthException;
+// import com.google.firebase.auth.FirebaseToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class TestController {
+
+  @GetMapping
+  @CrossOrigin("http://localhost:3000")
+  public String get(HttpServletRequest request) {
+    // String idToken = request.getHeader("Authorization");
+
+    // try {
+    //   FirebaseToken decodedToken =
+    //     FirebaseAuth.getInstance().verifyIdToken(idToken);
+    //   return "サーバー認証成功：java_firebase";
+    // } catch (FirebaseAuthException e) {
+    //   e.printStackTrace();
+    //   return "サーバー認証失敗";
+    // }
+    return "OK";
+  }
+}
