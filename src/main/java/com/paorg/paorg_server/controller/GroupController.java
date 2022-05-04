a8d8e653389e7cd0c056afa76622031628cc66fb
masa <masa.docomo.ne.jp@y7.dion.ne.jp>
2022-05-04 16:04:16 +0000

    POST受信確認

--- src/main/java/com/paorg/paorg_server/controller/GroupController.java
+++ src/main/java/com/paorg/paorg_server/controller/GroupController.java
@@ -2,9 +2,7 @@ package com.paorg.paorg_server.controller;
 
 import com.paorg.paorg_server.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,10 +10,64 @@ public class GroupController {
 
   private final GroupService groupService;
 
+  // 500エラー
+  // @PostMapping("/get_owner_list_with_ranking")
+  // public String getOwnerListWithRanking(@RequestBody String contents) {
+  //
+  //   return this.groupService.findOwnerListWithRanking(contents);
+  // }
+
+  // 400エラー
+  // @RequestMapping("/get_owner_list_with_ranking")
+  // public String getOwnerListWithRanking(@RequestBody String contents) {
+  //
+  //   return this.groupService.findOwnerListWithRanking(contents);
+  // }
+
+  // 成功：this.members = await this.$axios.$get('/get_owner_list_with_ranking')
+  // @RequestMapping("/get_owner_list_with_ranking")
+  // public String getOwnerListWithRanking() {
+  //
+  //   return "あああ";
+  // }
+
+  // 成功：await this.$axios.$post('/get_owner_list_with_ranking')
+  // @RequestMapping("/get_owner_list_with_ranking")
+  // public String getOwnerListWithRanking() {
+  //
+  //   return "あああ";
+  // }
+
+  // 成功：await this.$axios.$post('/get_owner_list_with_ranking', params)
+  // const params = {
+  //   userId: "ユーザーID"
+  // }
   @PostMapping("/get_owner_list_with_ranking")
   public String getOwnerListWithRanking(@RequestBody String contents) {
 
-    return this.groupService.findOwnerListWithRanking(contents);
+    return contents;
   }
+
+  // 下記でエンコードしている
+  // const params = new URLSearchParams()
+  // params.append('userId', 'ユーザーID')
+  // 成功：await this.$axios.$post('/get_owner_list_with_ranking', params)
+  // エンコードしたパラメータもデコードされる
+  // @PostMapping("/get_owner_list_with_ranking")
+  // public String getOwnerListWithRanking(@RequestParam("userId") String userId) {
+  //
+  //   return userId;
+  // }
+
+  // 下記でエンコードしている
+  // const params = new URLSearchParams()
+  // params.append('userId', 'ユーザーID')
+  // 失敗：await this.$axios.$post('/get_owner_list_with_ranking', params)
+  // エンコードされたまま受け取ってしまう
+  // @PostMapping("/get_owner_list_with_ranking")
+  // public String getOwnerListWithRanking(@RequestBody String contents) {
+  //
+  //   return contents;
+  // }
 
 }
